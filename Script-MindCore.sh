e062baa300119b25fd92fda63a201305e0e9bc32
KevinSampaio <kevin.sampaio@sptech.school>
2024-06-05 20:36:07 -0300

    assistente final

--- Script-MindCore.sh
+++ Script-MindCore.sh
@@ -43,15 +43,15 @@ fi
 
 
 
-echo "$(tput setaf 5)[Assistente Maya]: $(tput sgr0) $(tput setaf 10) Verificando se você possui o docker-compose"
+echo "$(tput setaf 5)[Assistente Maya]: $(tput sgr0) $(tput setaf 10) Estou verificando se você possui o docker-compose"
 
 docker-compose --version
 if [ $? = 0 ]
 then
-        echo "$(tput setaf 5)[Assistente Maya]: $(tput sgr0) $(tput setaf 10) Docker-compose instalado."
+        echo "$(tput setaf 5)[Assistente Maya]: $(tput sgr0) $(tput setaf 10) Possui Docker-compose instalado."
         sleep 2
 else
-        echo "$(tput setaf 5)[Assistente Maya]: $(tput sgr0) $(tput setaf 10) Não possui o docker-compose instalado"
+        echo "$(tput setaf 5)[Assistente Maya]: $(tput sgr0) $(tput setaf 10) Não possui o Docker-compose instalado"
         sleep 2
 
         sudo apt-get update
@@ -60,10 +60,10 @@ else
         sudo curl -L "https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
         sudo chmod +x /usr/local/bin/docker-compose
         sleep 2
-fi
 
-echo "$(tput setaf 5)[Assistente Maya]: $(tput sgr0) $(tput setaf 10) Criando o arquivo docker-compose.yml, ao final, clique Ctrl + C para terminar a criação do arquivo"
-sleep 2
+        echo "$(tput setaf 5)[Assistente Maya]: $(tput sgr0) $(tput setaf 10) Agora irei criar o arquivo docker-compose.yml, ao final, clique Ctrl + C para terminar a criação do arquivo"
+        sleep 2
+fi
 
 # Cria o arquivo docker-compose.yml
 cat <<EOL > docker-compose.yml
