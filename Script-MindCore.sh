9421e9a5357b5367925ef507dcf91c0c0e1fb64d
KevinSampaio <kevin.sampaio@sptech.school>
2024-06-05 20:13:24 -0300

    Assistente-v6

--- Script-MindCore.sh
+++ Script-MindCore.sh
@@ -1,3 +1,7 @@
+readonly USERNAME=mindcore
+readonly PASSWORD=mindcore123grupo6
+readonly DATABASE=MindCore
+
 echo "$(tput setaf 5)[Assistente Maya]: $(tput sgr0) $(tput setaf 10)Olá, eu sou a Maya, sua assistente virtual e vou te ajudar a iniciar nosso aplicativo!!"
 sleep 2
 
@@ -7,48 +11,60 @@ sleep 2
 sudo apt update
 sudo apt upgrade
 
-echo "$(tput setaf 5)[Assistente Maya]: $(tput sgr0) $(tput setaf 10) Agora irei instalar o docker e para poder rodar nossas aplicações no contêiner"
+echo "$(tput setaf 5)[Assistente Maya]: $(tput sgr0) $(tput setaf 10) Agora verificar se você tem o docker"
 sleep 2
 
-sudo apt update
-sudo apt upgrade -y
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+docker --version
 
-# Adicionar chave GPG e repositório Docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+if [ $? = 0 ]
+then
+        echo "$(tput setaf 5)[Assistente Maya]: $(tput sgr0) $(tput setaf 10) Docker Instalado"
+        sleep 2
+else
+        echo "$(tput setaf 5)[Assistente Maya]: $(tput sgr0) $(tput setaf 10) Docker não instalado"
+        sleep 2
 
-# Atualizar e instalar Docker
-sudo apt update
-sudo apt install docker-ce -y
+        sudo apt update
+        sudo apt upgrade -y
+        sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 
-# Verificar instalação e adicionar usuário ao grupo Docker
-echo "$(tput setaf 5)[Assistente Maya]: $(tput sgr0) $(tput setaf 10) Adicionando o docker ao grupo sudo..."
+        # Adicionar chave GPG e repositório Docker
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
-sudo systemctl status docker
-sudo usermod -aG docker ${USER}
+        # Atualizar e instalar Docker
+        sudo apt update
+        sudo apt install docker-ce -y
 
-echo "$(tput setaf 5)[Assistente Maya]: $(tput sgr0) $(tput setaf 10) Instalando o docker-compose..."
+        echo "$(tput setaf 5)[Assistente Maya]: $(tput sgr0) $(tput setaf 10) Adicionando o docker ao grupo sudo..."
+        sudo systemctl status docker
+        sudo usermod -aG docker ${USER}
+fi
 
-sudo apt-get update
-sudo apt-get install -y curl
 
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+
+echo "$(tput setaf 5)[Assistente Maya]: $(tput sgr0) $(tput setaf 10) Verificando se você possui o docker-compose"
 
 docker-compose --version
+if [ $? = 0 ]
+then
+        echo "$(tput setaf 5)[Assistente Maya]: $(tput sgr0) $(tput setaf 10) Docker-compose instalado."
+        sleep 2
+else
+        echo "$(tput setaf 5)[Assistente Maya]: $(tput sgr0) $(tput setaf 10) Não possui o docker-compose instalado"
+        sleep 2
 
-IMAGE_DB="helosalgado/atividadeso:v1"
-IMAGE_APP="helosalgado/atividadeso:app"
+        sudo apt-get update
+        sudo apt-get install -y curl
 
-# Baixa as imagens do Docker Hub, caso não estejam no sistema
-docker pull ${IMAGE_DB}
-docker pull ${IMAGE_APP}
-
-echo "$(tput setaf 5)[Assistente Maya]: $(tput sgr0) $(tput setaf 10) Imagens baixadas com sucesso."
-sleep 2
+        sudo curl -L "https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+        sudo chmod +x /usr/local/bin/docker-compose
+        sleep 2
+fi
 
 echo "$(tput setaf 5)[Assistente Maya]: $(tput sgr0) $(tput setaf 10) Criando o arquivo docker-compose.yml, ao final, clique Ctrl + C para terminar a criação do arquivo"
+sleep 2
+
 # Cria o arquivo docker-compose.yml
 cat <<EOL > docker-compose.yml
 version: '3.3'
@@ -56,7 +72,7 @@ services:
   bd:
     container_name: bd-mindcore
     build: .
-    image: ${IMAGE_DB}
+    image: helosalgado/atividadeso:v1
     restart: always
     ports:
       - "3307:3306"
@@ -64,7 +80,7 @@ services:
   java_app:
     container_name: javaApp
     build: .
-    image: ${IMAGE_APP}
+    image: helosalgado/atividadeso:app
     restart: always
     ports:
       - "8080:8080"
@@ -77,3 +93,30 @@ EOL
 
 echo "$(tput setaf 5)[Assistente Maya]: $(tput sgr0) $(tput setaf 10) Docker-compose.yml Criado"
 sleep 2
+
+echo "$(tput setaf 5)[Assistente Maya]: $(tput sgr0) $(tput setaf 10) Iniciando aplicação..."
+sleep 2
+
+sudo docker-compose up -d
+
+#docker start bd-mindcore > /dev/null
+
+#LOGIN=0
+#touch docker.env
+#while [ "$LOGIN" -eq 0 ]; do
+#echo "Digite o email"
+#read email
+
+#echo "Digite a senha"
+#read senha
+
+#query=$(sudo docker exec -it bd-mindcore bash -c "MYSQL_PWD="$PASSWORD" mysql --batch -u root -D "$DATABASE" -e 'SELECT idFunc, email, senha FROM Funcionario where email = \"$email\" AND senha = \"$senha\" LIMIT 1;'")
+
+#if [ -z "$query" ]; then
+#echo "Usuário não encontrados"
+
+#else
+
+#echo "Login efetuado com sucesso"
+#LOGIN=1
+#sleep 3
